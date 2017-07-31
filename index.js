f23984ab92c767333556d41fdfcaac1c68c43685
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-31 20:43:14 +0700

    xong

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.locals.title = 'Hello EJS';
+
+app.get('/', (req, res) => { 
+    res.render('index');
+});
+
+app.get('/products', (req, res) => {
+    res.render('products', { mang: ['NodeJS', 'ReactJS', 'React Native'] });
+});
+
 app.listen(3000, () => console.log('Server started!'))
+
+// killall -9 node
 
 reload(app);
